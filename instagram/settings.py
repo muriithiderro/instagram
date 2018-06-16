f84b2f4c25f5f275988d57aeb99c8103980d0404
muriithiderro <muriithiderrick56@gmail.com>
2018-06-16 21:48:01 +0300

    Add heroku url to allowed apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -27,7 +27,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'deryq-instagram.herokuapp.com']
 
 
 # Application definition
