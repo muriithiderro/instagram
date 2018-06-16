9781c42dbb83abe7152731acd5137cfd2a7294e3
muriithiderro <muriithiderrick56@gmail.com>
2018-06-16 23:37:48 +0300

    Add a user Model This allows creation of the user properties

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d',blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
