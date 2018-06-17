9a64b5a5c1758354de3885b2dd8282c352a4e492
muriithiderro <muriithiderrick56@gmail.com>
2018-06-17 18:48:41 +0300

    create the authentication templates to handle the various functionalities

--- account/forms.py
+++ account/forms.py
@@ -2,16 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
-
-class UserEditForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = '__all__'
-    
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
@@ -19,6 +12,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
+        #From User fields
         fields = ('username', 'first_name', 'email')
 
     def clean_password2(self):
@@ -27,3 +21,18 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('photo',)
+
+
+
--- account/models.py
+++ account/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     photo = models.ImageField(upload_to='users/%Y/%m/%d',blank=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+    
+    @property
+    def return_image(self):
+        return self.photo.url
