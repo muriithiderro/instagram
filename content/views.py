7671a372ea1b3cfccbc963affb7ddb16a2a3b288
muriithiderro <muriithiderrick56@gmail.com>
2018-06-17 18:53:05 +0300

    Add landing page view and methods
    Create the dashboard behaviour and the images  to be displayed
    Add search functionality view

--- content/views.py
+++ content/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Photo, Comment
+from .forms import PostImageForm,CommentForm
+from django.contrib import messages
+from django.db.models import Q
 
-# Create your views here.
+
+
+@login_required(login_url='/account/register/')
+def dashboard(request):
+    
+
+    
+
+    query = request.GET.get('q', None)
+    if query is not None:
+        photos = Photo.objects.filter(Q(title__contains=query.lower())|Q(description__contains=query.lower()))
+
+    else:
+        photos = Photo.objects.all()
+    
+    # id = int(request.POST.get('id',None))
+    # print(dir(request.user))
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST,instance=request.user)
+        if comment_form.is_valid():
+            print(comment_form)
+            comment_form.save()
+    else:
+        comment_form = CommentForm()
+    
+    return render(request, 'dashboard.html', {'section':'dashboard', 'photos':photos,'comment_form': comment_form, 'query':query})
+
+def add_photo(request):
+    if request.method == 'POST':
+        image_form = PostImageForm(data=request.POST, files=request.FILES,instance=request.user)
+        if image_form.is_valid():
+            # image_form.save()
+            instance = Photo(user=request.user, title=request.POST['title'], description=request.POST['description'],image=request.FILES['image'])
+            instance.save()
+            messages.success(request, 'Image post successifully added')
+            return redirect('profile')
+        else:
+            messages.success(request, 'Errors')
+    else:
+        image_form = PostImageForm()
+
+    return render(request, 'add_image.html', {'image_form': image_form})
+
+# @login_required
+# def comment(request):
+#     print(request.POST)
+#     if request.method == 'POST':
+#         comment_form = CommentForm(request.POST)
+       
+#         if comment_form.is_valid():
+#             pass
+#             # instance = Comment(name=request.user,body=request.POST['body'],photo=id)
+#             # instance.save()
+#     else:
+#         comment_form = CommentForm()
+#     return render(request, 'dashboard.html',{'comment_form': comment_form} )
+
